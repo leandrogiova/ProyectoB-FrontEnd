3a49c4f9fb765f39df57877f39bd3d4f8d389d65
LeandroGiovacchini <leandrogiovacchini>
2021-06-27 01:44:54 -0300

    Reestructurado nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MibarComponent } from './mibar/mibar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MibarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mibar/mibar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mibar',
+  templateUrl: './mibar.component.html',
+  styleUrls: ['./mibar.component.css']
+})
+export class MibarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
